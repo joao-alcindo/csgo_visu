bce94e507b376fda957e6d250d57d4d4c4a1e397
joao-alcindo <joao.az-rib@hotmail.com>
2021-12-12 19:55:07 -0300

    carregando os dados e fazendo análises

--- src/streamlit/functions.py
+++ src/streamlit/functions.py
@@ -8,6 +8,7 @@ def set_home():
 
 def set_players():
     st.write('Jogadores')
+    st.image('https://img-cdn.hltv.org/playerbodyshot/GJO4YI4EC6L1LpNFZX13eS.png?ixlib=java-2.1.0&w=400&s=1b49753162ebf20e6d3440f7e377ec55', width=250, output_format='png')
 
 def set_teams():
     st.write('Times')
